796825ce4cd6fc515d21c394cad9a57ea86ebfa5
aozeyu <3484766443@qq.com>
2024-03-03 15:16:03 +0800

    add code

--- src/test/java/com/imooc/springclouddemo2/WrapperTest.java
+++ src/test/java/com/imooc/springclouddemo2/WrapperTest.java
@@ -31,4 +31,23 @@ public class WrapperTest {
     int result = userMapper.delete(queryWrapper);
     System.out.println("delete return count = " + result);
   }
+
+
+  @Test
+  public void test5() {
+    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+    queryWrapper.select("name","age");
+    List<Map<String,Object>> maps = userMapper.selectMaps(queryWrapper);
+    maps.forEach(System.out::println);
+  }
+
+
+  @Test
+  public void test6() {
+    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+    queryWrapper.inSql("id","select id from user where id <= 3");
+
+    List<Object> objects = userMapper.selectObjs(queryWrapper);
+    objects.forEach(System.out::println);
+  }
 }
