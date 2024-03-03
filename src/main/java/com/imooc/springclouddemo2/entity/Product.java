2e5eeed46a29f9fa43c160d2937ee57357d4178e
aozeyu <3484766443@qq.com>
2024-03-03 15:04:22 +0800

    add code

--- src/main/java/com/imooc/springclouddemo2/entity/Product.java
+++ src/main/java/com/imooc/springclouddemo2/entity/Product.java
@@ -1,13 +1,30 @@
 package com.imooc.springclouddemo2.entity;
 
 
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.plugins.*;
+import com.baomidou.mybatisplus.extension.plugins.inner.*;
 import lombok.*;
+import org.springframework.context.annotation.*;
 
 @Data
 public class Product {
   private Long id;
   private String name;
   private Integer price;
+  @Version
   private Integer version;
+
+
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor()
+  {
+    MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+    //分页插件
+    mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+    //乐观锁插件
+    mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+    return mybatisPlusInterceptor;
+  }
 }
 
